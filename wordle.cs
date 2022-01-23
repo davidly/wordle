da8b373e71c94268939bddb4377d8231094f67f8
davidly <davidly_lee@hotmail.com>
2022-01-23 08:10:03 -0800

    add next word

--- wordle.cs
+++ wordle.cs
@@ -320,7 +320,7 @@ class Wordle
             "tapir", "troll",
             "rebus", "boost", "truss", "siege", "tiger", "banal", "slump", "crank", "gorge", "query",
             "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot",
-            "prick", "wince",
+            "prick", "wince", "crimp",
         };
 
         string [] userSolutions = { userSolution };
