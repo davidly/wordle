4e47b3dad7695a46b6cd0518c874dff21e2b7509
davidly <davidly_lee@hotmail.com>
2022-02-16 07:31:41 -0800

    add latest word

--- wordle.cs
+++ wordle.cs
@@ -614,7 +614,7 @@ class Wordle
             "drink", "favor", "abbey", "tangy", "panic", "solar", "shire", "proxy", "point", "robot",
             "prick", "wince", "crimp", "knoll", "sugar", "whack", "mount", "perky", "could", "wrung",
             "light", "those", "moist", "shard", "pleat", "aloft", "skill", "elder", "frame", "humor",
-            "pause", "elves", "ultra", "robin", "cynic", "aroma", 
+            "pause", "elves", "ultra", "robin", "cynic", "aroma", "caulk", 
         };
 
         string [] userSolutions = { userSolution };
