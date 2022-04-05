d8422f5f5df8f31c4922b7d2eec3863f46024cdf
davidly <davidly_lee@hotmail.com>
2022-04-05 07:35:49 -0700

    add latest words

--- wordle.cs
+++ wordle.cs
@@ -667,7 +667,9 @@ class Wordle
             "tacit", "other", "thorn", "trove", "bloke", "vivid", "spill", "chant", "choke", "rupee",
             "nasty", "mourn", "ahead", "brine", "cloth", "hoard", "sweet", "month", "lapse", "watch",
             "today", "focus", "smelt", "tease", "cater", "movie", "saute", "allow", "renew", "their",
-            "slosh", "purge", "chest", "depot", "epoxy", "nymph", "stove", 
+            "slosh", "purge", "chest", "depot", "epoxy", "nymph", "stove", "lowly", "snout", "trope",
+            "fewer", "shawl", "natal", 
+
         };
 
         string [] userSolutions = { userSolution };
