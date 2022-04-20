0cfbc88e85c304ceeea9932766ff64249b61c83d
davidly <davidly_lee@hotmail.com>
2022-04-20 07:03:18 -0700

    rm cairo from words.txt. add latest word

--- wordle.cs
+++ wordle.cs
@@ -669,7 +669,7 @@ class Wordle
             "today", "focus", "smelt", "tease", "cater", "movie", "saute", "allow", "renew", "their",
             "slosh", "purge", "chest", "depot", "epoxy", "nymph", "stove", "lowly", "snout", "trope",
             "fewer", "shawl", "natal", "comma", "foray", "scare", "stair", "black", "squad", "royal",
-            "chunk", "mince", "shame", "cheek", "ample", "flair", "foyer", 
+            "chunk", "mince", "shame", "cheek", "ample", "flair", "foyer", "cargo", 
         };
 
         string [] userSolutions = { userSolution };
