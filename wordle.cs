dc17a204add55759976e8f98cb06a15475365f96
davidly <davidly_lee@hotmail.com>
2022-07-10 06:53:57 -0700

    add latest words

--- wordle.cs
+++ wordle.cs
@@ -686,7 +686,7 @@ class Wordle
             "showy", "phase", "froth", "depth", "gloom", "flood", "trait", "girth", "piety", "goose",
             "float", "donor", "atone", "primo", "apron", "blown", "cacao", "loser", "input", "gloat",
             "awful", "brink", "smite", "beady", "rusty", "retro", "droll", "gawky", "pinto", "egret",
-            "lilac", 
+            "lilac", "sever", "field", "fluff", "agape", "stead", "berth", 
         };
 
         string [] userSolutions = { userSolution };
