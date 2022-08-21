f16e26428eccf95fff88f053e7329d10df8eced6
davidly <davidly_lee@hotmail.com>
2022-08-21 07:32:54 -0700

    latest words

--- wordle.cs
+++ wordle.cs
@@ -690,7 +690,7 @@ class Wordle
             "liver", "wedge", "roomy", "wacky", "flock", "angry", "trite", "aphid", "tryst", "midge",
             "power", "elope", "cinch", "motto", "stomp", "upset", "bluff", "cramp", "quart", "coyly",
             "youth", "rhyme", "buggy", "alien", "smear", "unfit", "patty", "cling", "glean", "label",
-            "hunky", "khaki", 
+            "hunky", "khaki", "poker", "gruel", "twice", "twang", "shrug", "treat", "waste",
         };
 
         string [] userSolutions = { userSolution };
