1e8e6407920c1f3c0b4d8442ea9c59d1a51aaddc
davidly <davidly_lee@hotmail.com>
2022-10-08 07:18:39 -0700

    latest words

--- wordle.cs
+++ wordle.cs
@@ -695,6 +695,7 @@ class Wordle
             "charm", "gully", "inter", "whoop", "taunt", "leery", "class", "theme", "lofty", "tibia",
             "booze", "alpha", "thyme", "doubt", "parer", "chute", "stick", "trice", "alike", "recap",
             "saint", "glory", "grate", "admit", "brisk", "soggy", "usurp", "scald", "scorn", "leave",
+            "twine", "sting", "bough", "marsh", "sloth", "dandy", "vigor", 
         };
 
         string [] userSolutions = { userSolution };
